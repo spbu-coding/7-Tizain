86ae6b50221a472c666f36c46027c0d5340f1a30
Timofey Zaynulin <dark-lord-6@mail.ru>
2021-03-20 13:56:52 +0300

    7th task

---
+++ src/task7.c
@@ -0,0 +1,103 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define  MAX_STRING_LEN 1000
+#define  MAX_STRING_NUM 100
+char *infile;
+FILE *inptr;
+
+int console_read(int argc, char* argv[]) {  // функция обработки команды с консоли
+    // проверка наличия названия текстового файла в команде пользователя
+	char *last_four = "init";
+	size_t len = strlen(argv[1]);
+	if (len > 4) {
+		last_four = &argv[1][len-4];
+	}
+	if (strcmp (last_four, ".txt") == 0) {
+		infile = argv[1]; 
+	} else {
+		return -1;
+	}
+    return 0;
+}
+
+size_t strings_count(void)  { // функция подсчета количества строк для вывода в поток
+	int c;
+	size_t lines_count = 0; 
+	while (!ferror(inptr) && !feof(inptr)) {
+		c = fgetc(inptr);
+		if (c == '\n') ++lines_count;
+	}
+	if (c != '\n') ++lines_count;
+	if (lines_count > MAX_STRING_NUM) lines_count = MAX_STRING_NUM; // Нам нужно 100 строк или меньше
+	return lines_count;
+}
+
+void delete_punctuation(char** strings_array, size_t array_size) { // функция удаления знаков препинания
+	char punct[7]=".,;:!?";
+  	for (size_t i = 0; i < array_size; i++) {
+		char* part; 
+		char whole_string[MAX_STRING_LEN + 2];
+		part = strtok(strings_array[i],punct);
+		while (part != NULL) {
+			strcat(whole_string, part);
+	    	part = strtok(NULL,punct);
+   		}
+		strcpy(strings_array[i],whole_string);
+		strcpy(whole_string,"");
+	}	
+}
+ 
+void sort(char** strings_array, size_t array_size) {  // функция сортировки строк по убыванию
+	char* tmp;
+    char flag;
+    do {
+        flag = 0;
+        for (size_t i = 1; i < array_size; i++) {
+            if (strcmp(strings_array[i-1],strings_array[i])<0) {
+                tmp = strings_array[i];
+                strings_array[i] = strings_array[i-1];
+                strings_array[i-1] = tmp;
+                flag = 1;
+            }
+        }
+    } while (flag);
+}
+
+int main(int argc, char* argv[]) {
+	if (argc < 2) return 0;
+    if (argc != 2) {
+        printf("The needed command format: task7 input.txt\n");
+        return 0;
+    }
+    // Чтение команды ввода с консоли и обработка ошибок в этой команде
+    if (console_read(argc, argv) == -1) {
+        return -1;
+    }
+    inptr = fopen(infile, "r");   // Открытие файла на чтение
+    if (inptr == NULL) {
+        printf("Could not open %s.\n", infile);
+        return -1;
+    }
+	size_t strings_number = strings_count();   // количество строк для вывода в поток
+	rewind(inptr);
+    // выделение памяти для массива, который будет содержать строки из файла и заполнение его
+    char **strings_array = (char**)malloc(strings_number * (MAX_STRING_LEN + 2)); //+2 (для fgets)возможные символы новой и нулевой строки
+    for (size_t i = 0; i < strings_number; i++) {
+        strings_array[i] = (char*)malloc(MAX_STRING_LEN + 2);
+        fgets(strings_array[i], MAX_STRING_LEN, inptr); 
+    }
+    fclose(inptr);
+	delete_punctuation(strings_array, strings_number);  // вызов функции удаления знаков препинания
+	if (strings_number > 1)  { // пропустим сортировку, если строка всего одна
+    	sort(strings_array, strings_number);  // вызов функции сортировки
+	}
+	for (size_t i = 0; i < strings_number; i++) {  // запись строк в поток вывода и освобождение памяти
+		fprintf(stdout, strings_array[i]);
+		free(strings_array[i]);
+    }
+    free(strings_array);    
+
+    return 0;
+}
